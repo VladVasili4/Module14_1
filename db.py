05da78fa20b19bd4ee59b32575d96cfb69c54487
VladVasili4 <dipol777@ya.ru>
2024-09-18 07:26:06 +0300

    Add files via upload

--- db.py
+++ db.py
@@ -3,20 +3,15 @@ import random
 
 connection = sqlite3.connect('database.db')
 cursor = connection.cursor()
-# cursor.execute("""
-# # CREATE TABLE IF NOT EXISTS Users(
-# # id INTEGER PRIMARY KEY,
-# # username TEXT NOT NULL,
-# # email TEXT NOT NULL,
-# # age INTEGER
-# # )
-# # """)
-# cursor.execute("CREATE INDEX IF NOT EXISTS idx_email ON Users(email)")
 
-cursor.execute("CREATE TABLES IF NOT EXISTS Users(
-               id
-               )
+cursor.execute("""CREATE TABLES IF NOT EXISTS Users(
+id INT
+username TEXT
+first_name TEXT
+block INT)""")
 
+def aser_add():
+    pass
 
 
 connection.commit()
